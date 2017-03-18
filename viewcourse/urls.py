1a34288097c99a6b60841b51da336eb58ba691ea
xiangling1994 <xn947387@dal.ca>
2017-03-18 11:30:25 -0300

    add login
    have bugs

--- viewcourse/urls.py
+++ viewcourse/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     url(r'^course/(?P<pk>[0-9]+)/$', views.course_detail, name='course_detail'),
     url(r'^course/new/$', views.course_new, name='course_new'),
     url(r'^course/(?P<pk>\d+)/comment/$', views.comment_new, name='comment_new'),
+    url(r'^login/', views.loginview, name='loginview'),
+    url(r'^auth/', views.auth_and_login, name="auth_login"),
+    url(r'^signup/', views.sign_up_in, name="signup"),
 ]
--- viewcourse/views.py
+++ viewcourse/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from .models import addcourse
 from .forms import PostForm, CommentForm
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,render_to_response, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import requires_csrf_token
+
 
 # Create your views here.
 def course_list(request):
@@ -41,3 +45,41 @@ def comment_new(request, pk):
 def deliver(request):
     dada = "dadada"
     return render(request, 'viewcourse/course_list.html', {'dada': dada,})
+
+
+@requires_csrf_token
+def loginview(request):
+    c = {}
+    return render_to_response('login.html',c)
+
+def auth_and_login(request, onsuccess='/', onfail='/login/'):
+    user = authenticate(username=request.POST['email'], password=request.POST['password'])
+    if user is not None:
+        login(request, user)
+        return redirect(onsuccess)
+    else:
+        return redirect(onfail)
+
+
+def sign_up_in(request):
+    post = request.POST
+    if  post=={}:
+        return redirect("/signup/")
+    if not user_exists(post['email']):
+        user = create_user(username=post['email'], email=post['email'], password=post['password'])
+        return redirect("/")
+    else:
+        return auth_and_login(request)
+
+
+def create_user(username, email, password):
+    user = User(username=username, email=email)
+    user.set_password(password)
+    user.save()
+    return user
+
+def user_exists(username):
+    user_count = User.objects.filter(username=username).count()
+    if user_count == 0:
+        return False
+    return True
